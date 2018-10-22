67f0f4c39db292c647adee2fa4852015f52060aa
AlexanderPhan <42187090+AlexanderPhan@users.noreply.github.com>
2018-10-22 15:56:47 -0500

    Iriswork.js

---
+++ Iriswork.js
@@ -0,0 +1,131 @@
+/*
+*	Load Required Node Modules
+*/
+
+var spotify = require('spotify');
+/*
+* 	Read in command line arguments
+*/
+
+// Read in the command line arguments
+var cmdArgs = process.argv;
+
+// The LIRI command will always be the second command line argument
+var liriCommand = cmdArgs[2];
+
+// The parameter to the LIRI command may contain spaces
+var liriArg = '';
+for (var i = 3; i < cmdArgs.length; i++) {
+	liriArg += cmdArgs[i] + ' ';
+}
+
+
+// spotifySong will retrieve information on a song from Spotify
+function spotifySong(song) {
+	// Append the command to the log file
+	fs.appendFile('./log.txt', 'User Command: node Iriswork.js spotify-this-song ' + song + '\n\n', (err) => {
+		if (err) throw err;
+    });
+    
+	// If no song is provided, LIRI defaults to 'The Sign' by Ace Of Base
+	var search;
+	if (song === '') {
+		search = 'The Sign Ace Of Base';
+	} else {
+		search = song;
+	}
+
+	spotify.search({ type: 'track', query: search}, function(error, data) {
+	    if (error) {
+			var errorStr1 = 'ERROR: Retrieving Spotify track -- ' + error;
+
+			// Append the error string to the log file
+			fs.appendFile('./log.txt', errorStr1, (err) => {
+				if (err) throw err;
+				console.log(errorStr1);
+			});
+			return;
+	    } else {
+			var songInfo = data.tracks.items[0];
+			if (!songInfo) {
+				var errorStr2 = 'ERROR: No song info retrieved, please check the spelling of the song name!';
+
+				// Append the error string to the log file
+				fs.appendFile('./log.txt', errorStr2, (err) => {
+					if (err) throw err;
+					console.log(errorStr2);
+				});
+				return;
+			} else {
+				// Pretty print the song information
+				var outputStr = '------------------------\n' + 
+								'Song Information:\n' + 
+								'------------------------\n\n' + 
+								'Song Name: ' + songInfo.name + '\n'+ 
+								'Artist: ' + songInfo.artists[0].name + '\n' + 
+								'Album: ' + songInfo.album.name + '\n' + 
+								'Preview Here: ' + songInfo.preview_url + '\n';
+
+				// Append the output to the log file
+				fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+					if (err) throw err;
+					console.log(outputStr);
+				});
+			}
+	    }
+	});
+}
+
+
+// doAsYerTold will read in a file to determine the desired command and then execute
+function doAsYerTold() {
+	fs.appendFile('./log.txt', 'User Command: node liri.js do-what-it-says\n\n', (err) => {
+		if (err) throw err;
+	});
+
+	// Read in the file containing the command
+	fs.readFile('./sample.txt', 'utf8', function (error, data) {
+		if (error) {
+			console.log('ERROR: Reading sample.txt -- ' + error);
+			return;
+		} else {
+			// Split out the command name and the parameter name
+			var cmdString = data.split(',');
+			var command = cmdString[0].trim();
+			var param = cmdString[1].trim();
+
+			switch(command) {
+
+				case 'spotify-this-song':
+					spotifySong(param);
+					break;
+
+			}
+		}
+	});
+}
+
+// Determine which LIRI command is being requested by the user
+if (liriCommand === `spotify-this-song`) {
+	spotifySong(liriArg);
+} 
+ else {
+	// Append the command to the log file
+	fs.appendFile('./log.txt', 'User Command: ' + cmdArgs + '\n\n', (err) => {
+		if (err) throw err;
+
+		// If the user types in a command that LIRI does not recognize, output the Usage menu 
+		// which lists the available commands.
+		outputStr = 'Usage:\n' + 
+				   '    node liri.js my-tweets\n' + 
+				   '    node liri.js spotify-this-song "<song_name>"\n' + 
+				   '    node liri.js movie-this "<movie_name>"\n' + 
+				   '    node liri.js do-what-it-says\n';
+
+		// Append the output to the log file
+		fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+			if (err) throw err;
+			console.log(outputStr);
+		});
+	});
+}
